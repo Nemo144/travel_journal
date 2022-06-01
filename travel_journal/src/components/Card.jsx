f26c3719e2bf5c8556e1b574484841610c6441e9
Nnamdi Okeke <onnamdialex@gmail.com>
2022-06-01 23:58:40 +0100

    :construction: card still under wip

--- travel_journal/src/components/Card.jsx
+++ travel_journal/src/components/Card.jsx
@@ -3,7 +3,7 @@ import data from "../constants/data";
 
 export const Card = () => {
   return (
-    <div className="flex flex-col flex-grow justify-between items-start">
+    <div className="flex flex-col flex-grow mt-12 border-b ml-7 justify-between items-start">
       <div className="flex flex-row">
         {data.map((item) => (
           <div>
@@ -15,19 +15,86 @@ export const Card = () => {
           </div>
         ))}
         {data.map((item) => (
-          <div className="flex mt-12 ml-5">
+          <div className="flex flex-col mt-12 ml-5">
             <h1>📍{item.location}</h1>
             {data.map((item) => (
-              <a
-                href={item.googleMapsUrl}
-                className="ml-2 text-zinc-400 font-light underline"
-              >
-                View on google maps
-              </a>
+              <div className="absolute pl-16">
+                <a
+                  href={item.googleMapsUrl}
+                  className="ml-2 mb-1 text-zinc-400 font-light underline"
+                >
+                  View on google maps
+                </a>
+              </div>
             ))}
           </div>
         ))}
+        {data.map((item) => (
+          <div className="my-20 -mx-16 pl-2">
+            <h1 className="text-4xl font-bold">{item.title}</h1>
+          </div>
+        ))}
+        {data.map((item) => (
+          <div className="my-36 -mx-28 pl-0">
+            <h2 className="text-sm font-bold">
+              {item.startDate} - {item.endDate}
+            </h2>
+          </div>
+        ))}
+        {data.map((item) => (
+          <div className=" my-44 -mx-28 pl-9 ">
+            <p className="font-light">{item.description1}</p>
+            <p className="font-light">{item.description2}</p>
+            <p className="font-light">{item.description3}</p>
+          </div>
+        ))}
       </div>
+
+      {/* <div className="flex flex-row">
+        {data.map((item) => (
+          <div>
+            <img
+              src={item.imageUrl1}
+              alt="img"
+              className="w-80 h-96 rounded-md mt-5 ml-7 "
+            />
+          </div>
+        ))}
+        {data.map((item) => (
+          <div className="flex flex-col mt-12 ml-5">
+            <h1>📍{item.location1}</h1>
+            {data.map((item) => (
+              <div className="absolute pl-16">
+                <a
+                  href={item.googleMapsUrl1}
+                  className="ml-2 mb-1 text-zinc-400 font-light underline"
+                >
+                  View on google maps
+                </a>
+              </div>
+            ))}
+          </div>
+        ))}
+        {data.map((item) => (
+          <div className="my-20 -mx-16 pl-2">
+            <h1 className="text-4xl font-bold">{item.title1}</h1>
+          </div>
+        ))}
+        {data.map((item) => (
+          <div className="my-36 -mx-28 pl-0">
+            <h2 className="text-sm font-bold">
+              {item.startDate1} - {item.endDate1}
+            </h2>
+          </div>
+        ))}
+        {data.map((item) => (
+          <div className=" my-44 -mx-28 pl-9 ">
+            <p className="font-light">{item.description4}</p>
+            <p className="font-light">{item.description5}</p>
+            <p className="font-light">{item.description6}</p>
+          </div>
+        ))}
+      </div> */}
     </div>
   );
 };
--- travel_journal/src/constants/data.js
+++ travel_journal/src/constants/data.js
@@ -5,8 +5,27 @@ export default [
     googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
     startDate: "12 Jan, 2021",
     endDate: "24 Jan, 2021",
-    description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    description1:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776",
+    description2:
+      "meters (12,380 feet). Mount Fuji is the single most popular tourist",
+    description3: "site in Japan, for both Japanese and foreign tourists.",
+
     imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
   },
+
+  // {
+  //   title1: "Sydney Opera House",
+  //   Location1: "Australia",
+  //   googleMapsUrl1: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+  //   startDate1: "27 May, 2021",
+  //   endDate1: "8 Jun, 2021",
+  //   description4:
+  //     "The Sydney Opera House is a multi-venue performing arts centre",
+  //   description5:
+  //     "in Sydney. Located on the banks of the Sydney Harbour, it is often",
+  //   description6:
+  //     "regarded as one of the 20th century's most famous and distinctive buildings",
+  //   imageUrl1: "https://source.unsplash.com/JmuyB_LibRo",
+  // },
 ];
